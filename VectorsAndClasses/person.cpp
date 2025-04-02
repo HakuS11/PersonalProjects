b60dc27af012ad155180aceff434631b9d5360d9
HakuS11 <131491168+HakuS11@users.noreply.github.com>
2025-04-01 18:30:21 -0700

    Update person.cpp

--- VectorsAndClasses/person.cpp
+++ VectorsAndClasses/person.cpp
@@ -1,25 +1,33 @@
 #include <iostream>
+#include <algorithm>
 #include "person.h"
 
-Person::Person(std::string new_name) : name(new_name) {}
+Person::Person(const std::string& new_name) : name(new_name) {}
 
 Person::~Person() {}
 
-void Person::addToInventory(std::string item) {
+void Person::addToInventory(const std::string& item) {
     inventory.push_back(item);
 }
 
-void Person::removeFromInventory(std::string item) {
+void Person::removeFromInventory(const std::string& item) {
     auto it = std::find(inventory.begin(), inventory.end(), item);
     if (it != inventory.end()) {
         inventory.erase(it);
+        std::cout << "Removed \"" << item << "\" from inventory.\n";
+    } else {
+        std::cout << "Item \"" << item << "\" not found in inventory.\n";
     }
 }
 
 void Person::printInventory() const {
     std::cout << name << "'s Inventory: ";
-    for (const std::string& item : inventory) {
-        std::cout << item << " ";
+    if (inventory.empty()) {
+        std::cout << "No items in inventory.";
+    } else {
+        for (const std::string& item : inventory) {
+            std::cout << item << " ";
+        }
     }
     std::cout << std::endl;
 }
